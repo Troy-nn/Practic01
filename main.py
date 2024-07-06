107228047495be22c3ac251bc2aa44e3fa1bbebd
Troy-nn <nrgnnrf@gmail.com>
2024-07-06 08:32:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1st program
+print(9.0 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print((1234 % 1000 // 10) + (5678 % 1000 // 10))
+
+#4 th program
+print(bool(13.42 * 100 % 1300) or (42.13 // 1))
